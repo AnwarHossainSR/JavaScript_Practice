d474063e8cfb9628faba403b4aa847b440301d09
AnwarHossainSR <43861146+AnwarHossainSR@users.noreply.github.com>
2020-03-24 20:28:47 +0600

    Add files via upload

---
+++ Javascript_Practice/DOM/dom.js
@@ -0,0 +1,9 @@
+
+var element;
+//element=document.head;
+//element=document.all;
+//element=document.URL;
+//element=document.getElementsByTagName('header');
+//element=document.getElementsByClassName('header');
+element=document.getElementsByTagName('ul');
+console.log(element); 
